5e1462be92b730c75b551dcb4f35bb83b8873bb1
grishmaa7 <gisme7@gmail.com>
2026-08-03 12:13:41 +0545

    test

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('students/<int:id>/', views.detail, name='student-detail'),
 ]
 
-path('students/<int:id>/', views.detail),
+
--- students/views.py
+++ students/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import HttpResponse
 def home(request):
     return HttpResponse("Hewwwooooo, 'm grrrshma!")
 
+
+
 def detail(request, id):
     return HttpResponse(f"You asked for student #{id}")
 
